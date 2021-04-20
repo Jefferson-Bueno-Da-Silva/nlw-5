0f420b932f5aae436835fd6597f9bb51e52494f7
Jefferson-Bueno-Da-Silva <jeffersonbuenoprofissional@gmail.com>
2021-04-20 10:43:43 -0300

    estruturação do banco de dados, typeORM e SQLite

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,10 @@
-import express, { response } from 'express';
-const app = express();
-/**
- * GET = Buscas
- * POST = Criação
- * PUT = Alteração
- * DELETE = Deletar
- * PATCH = Alterar uma informação especifica 
- */
-app.get('/', (req, res) => {
-  return res.json({
-    message: "Olá NLW 05"
-  });
-});
+import express from 'express';
 
-app.post("/", (req, res) =>{
-  return res.json({ 
-    message : "Usuário Salvo com sucesso"
-  });
-});
+import "./database";
+import { routes } from './routes';
+
+const app = express();
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => console.log("Server is running on port: 3333") );
