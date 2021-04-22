380eb01ae6dbb27f505745b27508e21f988154a3
Jefferson-Bueno-Da-Silva <jeffersonbuenoprofissional@gmail.com>
2021-04-22 10:01:11 -0300

    list messages by user

--- src/controllers/MessagesController.ts
+++ src/controllers/MessagesController.ts
@@ -4,8 +4,8 @@ import { MessagesService } from "../Services/MessagesService";
 export class MessagesController {
   async create(request: Request , response: Response ){
     const { admin_id, text, user_id } = request.body;
-    const messagesService = new MessagesService();
     
+    const messagesService = new MessagesService();
     const message = await messagesService.create({
       admin_id, 
       text, 
@@ -13,5 +13,14 @@ export class MessagesController {
     });
     
     return response.json(message);
+  }
+
+  async showByUser(request:Request, response: Response){
+    const { id } = request.params;
+    
+    const messagesService = new MessagesService();
+    const list = await messagesService.listByUser(id);
+    
+    return response.json(list);
   }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -5,12 +5,17 @@ import { UsersController } from "./controllers/UsersController";
 
 const routes = Router();
 
+//settings
 const settingsController = new SettingsController();
-const usersController = new UsersController();
-const messagesController = new MessagesController();
-
 routes.post( "/settings", settingsController.create )
+
+//users
+const usersController = new UsersController();
 routes.post( "/users", usersController.create );
+
+// Messages
+const messagesController = new MessagesController();
 routes.post( "/messages", messagesController.create );
+routes.get("/messages/:id", messagesController.showByUser);
 
 export { routes };
