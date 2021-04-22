b8cf9d6cf23d94cc981f7e9b8cdb33cd68255bfe
Jefferson-Bueno-Da-Silva <jeffersonbuenoprofissional@gmail.com>
2021-04-22 11:40:45 -0300

    configurando o websocket e o frontend da aplicação

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,38 @@
 import express from 'express';
+import { createServer } from "http";
+import { Server, Socket } from "socket.io"
+import path from "path";
 
 import "./database";
 import { routes } from './routes';
 
 const app = express();
+
+// Configuração para utilizar o html da pasta Public
+app.use( 
+  express.static( 
+    path.join( __dirname, "..", "public" ) 
+  ) 
+);
+app.set("views", path.join(__dirname, "..", "public") );
+app.engine("html", require("ejs").renderFile);
+app.set( "view engine", "html" );
+//Rota para interpretar o arquivo html
+app.get("/pages/client", (request, response) => {
+  return response.render("html/client.html");
+});
+
+// Configurando o Socket
+const http = createServer(app); //Criando Protocolo HTTP
+const io = new Server(http); //Criando Protocolo WS
+
+io.on("connection", (socket: Socket) => {
+  console.log("Se Conectou ", socket.id);
+});
+
+// Configurando o http do express
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => console.log("Server is running on port: 3333") );
+//Subindo o servidor na porta 3333;
+http.listen(3333, () => console.log("Server is running on port: 3333") );
